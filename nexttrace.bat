d7f791a68ff4591bb03949df3fb8285f9155b4a1
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-02-22 14:15:12 +0800

    Update nexttrace.bat

--- nexttrace.bat
+++ nexttrace.bat
@@ -16,10 +16,6 @@ if "%target%"=="" (
 
 )
 
-rem echo Adding proxy server %NEXTTRACE_PROXY%...
-
-rem set NEXTTRACE_PROXY=socks5://127.0.0.1:10808
-
 echo Running %EXEC_NAME% with pow provider %pow_provider% and target %target%...
 
 %EXEC_NAME% --pow-provider %pow_provider% %target%
