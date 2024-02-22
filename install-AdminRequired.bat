8ba8c849c2ddba0ed2a7516413848011a990bc8c
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-02-22 13:39:59 +0800

    Create install-AdminRequired.bat

---
+++ install-AdminRequired.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem The executable file name can be modified according to the actual situation.
+
+set EXEC_NAME=nexttrace_windows_amd64.exe
+
+echo Copy file . . .
+
+cd /d %~dp0
+
+copy /y .\%EXEC_NAME% %WINDIR%\%EXEC_NAME%
+
+echo Add ICMP allow rule in ipv4 and ipv6 . . .
+
+netsh advfirewall firewall delete rule name="All ICMP v4 (Nexttrace)"
+
+netsh advfirewall firewall delete rule name="All ICMP v6 (Nexttrace)"
+
+netsh advfirewall firewall add rule name="All ICMP v4 (Nexttrace)" dir=in action=allow protocol=icmpv4:any,any
+
+netsh advfirewall firewall add rule name="All ICMP v6 (Nexttrace)" dir=in action=allow protocol=icmpv6:any,any
+
+echo Finished!
+
+pause > nul
