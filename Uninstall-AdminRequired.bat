6e99d3155aaca02d9a493f49bf62ac899747e12a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-06-29 13:07:38 +0800

    Update Uninstall-AdminRequired.bat

--- Uninstall-AdminRequired.bat
+++ Uninstall-AdminRequired.bat
@@ -12,6 +12,8 @@ del /f /s /q %WINDIR%\nexttraceo.bat
 
 del /f /s /q %WINDIR%\nexttracex.bat
 
+del /f /s /q %WINDIR%\ipinfoLocal.mmdb
+
 echo Delete ICMP allow rule in ipv4 and ipv6 . . .
 
 netsh advfirewall firewall delete rule name="All ICMP v4 (Nexttrace)"
