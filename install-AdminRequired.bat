6a778f9cc370f57079f83ff2c5be96d9ca1c21f9
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-07-31 17:55:23 +0800

    Update install-AdminRequired.bat

--- install-AdminRequired.bat
+++ install-AdminRequired.bat
@@ -2,6 +2,8 @@
 
 rem The executable file name can be modified according to the actual situation.
 
+set ALL_PROXY=socks5://127.0.0.1:10808
+
 set EXEC_NAME=nexttrace_windows_amd64.exe
 
 echo Copy file . . .
